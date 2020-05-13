2b9004a9e0e088e53f2cdc89b55d05f3137fd31d
jomari <jomarialang.com>
2020-05-13 13:40:21 +0100

    fix You're using the staticfiles app without having set the STATIC_ROOT setting to a filesystem path

--- server/settings.py
+++ server/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend', 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR)
 MEDIA_URL = '/media/'
