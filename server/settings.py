6d6f188c549b69489130c9c0d63d1a6eee3e3aa1
jomari <jomarialang.com>
2020-05-14 13:28:13 +0100

    feat integrated travis

--- server/settings.py
+++ server/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'config',
     'checkout',
     'services',
-    'projects'
+    'projects',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -131,12 +132,32 @@ USE_L10N = True
 
 USE_TZ = True
 
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000'
+}
+
+AWS_STORAGE_BUCKET_NAME = 'jomari-designs-2020'
+AWS_S3_REGION_NAME = 'eu-west-1'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'build/static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR)
 MEDIA_URL = '/media/'
+
+
+MEDIAFILES_LOCATION = 'media'
+DEFAUL_FILE_STORAGE = 'custom_storages.MediaStorage'
