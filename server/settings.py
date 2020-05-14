61fa17de41b0a7c01b642f0f77036596cbd6cd95
jomari <jomarialang.com>
2020-05-14 14:11:35 +0100

    fix added heroku app to allowed hosts in django settings

--- server/settings.py
+++ server/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['jomari-designs-2020.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['jomari-designs-app.herokuapp.com', 'localhost']
 
 
 # Application definition
