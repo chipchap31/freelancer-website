f1445e98cd8cfdcfbf46525212ee3e6363926270
jomari <jomarialang.com>
2020-05-19 17:08:48 +0100

    test added a service in travis.yml

--- server/urls.py
+++ server/urls.py
@@ -4,13 +4,9 @@ from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
-from rest_framework_swagger.views import get_swagger_view
-
-schema_view = get_swagger_view(title='Pastebin API')
-
 
 urlpatterns = [
-    path('api_documentation/', schema_view),
+
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
     path('api/', include('accounts.urls')),
